23a227dbd40e4c1fc6f86a0a27eefc4a2477833f
Outrider99 <outrider.w@gmail.com>
2017-09-07 11:57:18 -0600

    Created runner and controller files and completed runner file

---
+++ src/process/controller/ProcessRunner.java
@@ -0,0 +1,10 @@
+package process.controller;
+
+public class ProcessRunner
+{
+	public static void main(String [] args)
+	{
+		ProcessController app = new ProcessController();
+		app.start();
+	}
+}
